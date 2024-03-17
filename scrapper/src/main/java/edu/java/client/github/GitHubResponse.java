4c025d95b8809b3b2f18886d11c776774c7c7ee2
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-17 23:19:09 +0300

    Minor update

--- scrapper/src/main/java/edu/java/client/github/GitHubResponse.java
+++ scrapper/src/main/java/edu/java/client/github/GitHubResponse.java
@@ -3,6 +3,8 @@ package edu.java.client.github;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record GitHubResponse(
+    // hw5 bonus task
+
     long id,
     String name,
     @JsonProperty("updated_at") String updatedAt,
