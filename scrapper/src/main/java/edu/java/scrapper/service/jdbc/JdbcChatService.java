5fba97f31672a2fde65417e1524794be6afb52df
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-18 17:22:45 +0300

    Create jdbc chat service implementation

---
+++ scrapper/src/main/java/edu/java/scrapper/service/jdbc/JdbcChatService.java
@@ -0,0 +1,24 @@
+package edu.java.scrapper.service.jdbc;
+
+import edu.java.scrapper.domain.dto.ChatDto;
+import edu.java.scrapper.domain.repository.jdbc.JdbcChatRepository;
+import edu.java.scrapper.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JdbcChatService implements ChatService {
+    private final JdbcChatRepository jdbcChatRepository;
+
+    @Override
+    public void register(long tgChatId) {
+        ChatDto chat = new ChatDto(tgChatId, null);
+        jdbcChatRepository.add(chat);
+    }
+
+    @Override
+    public void unregister(long tgChatId) {
+        jdbcChatRepository.remove(new ChatDto(tgChatId, null));
+    }
+}
