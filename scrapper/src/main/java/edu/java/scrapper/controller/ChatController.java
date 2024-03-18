bf28fb43d0000a1a3bb42e1a6b01fb45e65ab476
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-18 17:56:12 +0300

    Split the scraper controller into a link controller and a chat controller. Add method bodies

---
+++ scrapper/src/main/java/edu/java/scrapper/controller/ChatController.java
@@ -0,0 +1,26 @@
+package edu.java.scrapper.controller;
+
+import edu.java.scrapper.controller.api.ChatApi;
+import edu.java.scrapper.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class ChatController implements ChatApi {
+    private final ChatService service;
+
+    @Override
+    public void tgChatIdDelete(Long id) {
+        log.info("Delete tg chat {}", id);
+        service.unregister(id);
+    }
+
+    @Override
+    public void tgChatIdPost(Long id) {
+        log.info("Add tg chat {}", id);
+        service.register(id);
+    }
+}
