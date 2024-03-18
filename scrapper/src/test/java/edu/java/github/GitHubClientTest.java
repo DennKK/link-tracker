e464bf62074152ecf1c04dfb534773a2065a94aa
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-18 18:06:44 +0300

    Update imports

--- scrapper/src/test/java/edu/java/github/GitHubClientTest.java
+++ scrapper/src/test/java/edu/java/github/GitHubClientTest.java
@@ -1,6 +1,8 @@
 package edu.java.github;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import edu.java.client.github.GitHubClient;
+import edu.java.client.github.GitHubResponse;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
