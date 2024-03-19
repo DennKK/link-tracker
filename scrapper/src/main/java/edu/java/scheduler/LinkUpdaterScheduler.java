7cafb3ad28b1dec1e4f1f00802efa470ff9b2534
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-19 20:51:00 +0300

    Add a body for the update method

--- scrapper/src/main/java/edu/java/scheduler/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/scheduler/LinkUpdaterScheduler.java
@@ -1,16 +1,52 @@
 package edu.java.scheduler;
 
+import edu.java.client.github.GitHubClient;
+import edu.java.client.tgbot.BotClient;
+import edu.java.scrapper.domain.dto.LinkDto;
+import edu.java.scrapper.service.jdbc.JdbcLinkService;
+import java.time.OffsetDateTime;
+import java.util.Collection;
 import java.util.logging.Logger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 @EnableScheduling
-public class LinkUpdaterScheduler {
+@Service
+@RequiredArgsConstructor
+public class LinkUpdaterScheduler implements LinkUpdater {
+    private final JdbcLinkService jdbcLinkService;
+    private final GitHubClient gitHubClient;
+    private final BotClient botClient;
+
+    @Value("${app.scheduler.update-frequency}")
+    private int updateFrequency;
 
     private static final Logger LOGGER = Logger.getLogger(LinkUpdaterScheduler.class.getName());
 
     @Scheduled(fixedDelayString = "${app.scheduler.interval}")
     public void update() {
         LOGGER.info("Running link update task...");
+
+        Collection<LinkDto> oldLinks = jdbcLinkService.getOldLinks(updateFrequency);
+
+        for (LinkDto link : oldLinks) {
+            OffsetDateTime lastTimeUpdated = getLastUpdateTime(link);
+            if (lastTimeUpdated.isAfter(link.getUpdatedAt())) {
+                botClient.sendUpdateToBot(link);
+                link.setUpdatedAt(lastTimeUpdated);
+                jdbcLinkService.updateLink(link);
+            }
+        }
+    }
+
+    // TODO: url parser
+    // В предыдущих заданиях нигде не было необходимости парсить ссылки
+    // Хотелось бы отличать ссылку на stackoverflow и github друга от друга и от других ссылок
+    // Распарсиватель ссылок пока не готов, поэтому оставляю заглушку
+    private OffsetDateTime getLastUpdateTime(LinkDto link) {
+        return OffsetDateTime.parse(gitHubClient.getGitHubResponse("DennKK", "link-tracker").updatedAt());
     }
 }
