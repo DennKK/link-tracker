6561bb7f76e225926c13634451bcb919305177ff
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-23 14:10:42 +0300

    Rename package with jooq auto-generated code

--- scrapper-jooq/src/main/java/JooqCodegen.java
+++ scrapper-jooq/src/main/java/JooqCodegen.java
@@ -33,7 +33,7 @@ public class JooqCodegen {
             .withPojos(true);
 
         Target target = new Target()
-            .withPackageName("edu.java.scrapper.domain.jooq")
+            .withPackageName("edu.java.scrapper.domain.jooq_generated")
             .withDirectory("scrapper/src/main/java");
 
         Configuration configuration = new Configuration()
