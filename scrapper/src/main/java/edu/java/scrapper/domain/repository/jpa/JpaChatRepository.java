4c1cbf8f720037a95e51acd368a16de0cc8ff154
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-24 16:14:30 +0300

    Create jpa chat repository

---
+++ scrapper/src/main/java/edu/java/scrapper/domain/repository/jpa/JpaChatRepository.java
@@ -0,0 +1,9 @@
+package edu.java.scrapper.domain.repository.jpa;
+
+import edu.java.scrapper.domain.entity.ChatEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JpaChatRepository extends JpaRepository<ChatEntity, Long> {
+}
