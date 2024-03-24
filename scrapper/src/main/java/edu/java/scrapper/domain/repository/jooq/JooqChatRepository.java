61cb0b2c771909cf182c8099c98d49faa85b267a
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-24 12:34:43 +0300

    Implement ChatRepository with jooq

--- scrapper/src/main/java/edu/java/scrapper/domain/repository/jooq/JooqChatRepository.java
+++ scrapper/src/main/java/edu/java/scrapper/domain/repository/jooq/JooqChatRepository.java
@@ -2,26 +2,30 @@ package edu.java.scrapper.domain.repository.jooq;
 
 import edu.java.scrapper.domain.dto.ChatDto;
 import java.util.List;
+import edu.java.scrapper.domain.repository.ChatRepository;
 import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
-import static edu.java.scrapper.domain.jooq.Tables.CHATS;
-import static edu.java.scrapper.domain.jooq.Tables.LINKS;
+import static edu.java.scrapper.domain.jooq_generated.Tables.CHATS;
+import static edu.java.scrapper.domain.jooq_generated.Tables.LINKS;
 
 @RequiredArgsConstructor
-public class JooqChatRepository {
+public class JooqChatRepository implements ChatRepository {
     private final DSLContext dslContext;
 
+    @Override
     public void add(ChatDto chat) {
         dslContext.insertInto(CHATS)
             .set(CHATS.REGISTERED_AT, chat.getRegisteredAt()).execute();
     }
 
+    @Override
     public List<ChatDto> findAll() {
         return dslContext.selectFrom(CHATS).fetchInto(ChatDto.class);
     }
 
+    @Override
     public int remove(ChatDto chat) {
         return dslContext.delete(CHATS)
-            .where(LINKS.LINK_ID.eq(chat.getChatId().intValue())).execute();
+            .where(LINKS.LINK_ID.eq(chat.getChatId())).execute();
     }
 }
