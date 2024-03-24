b43002849816a36c020fbf3af9fec51406a2b210
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-24 23:16:10 +0300

    Create chat factory

---
+++ scrapper/src/main/java/edu/java/scrapper/service/factory/ChatFactory.java
@@ -0,0 +1,15 @@
+package edu.java.scrapper.service.factory;
+
+import edu.java.scrapper.domain.dto.ChatDto;
+import edu.java.scrapper.domain.entity.ChatEntity;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ChatFactory {
+    public static ChatDto entityToDto(ChatEntity chatEntity) {
+        ChatDto chatDto = new ChatDto();
+        chatDto.setChatId(chatEntity.getChatId());
+        chatDto.setRegisteredAt(chatEntity.getRegisteredAt());
+        return chatDto;
+    }
+}
