134d3c23c9a9c855b56d73e31af93b63cb0d16a4
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-24 16:16:34 +0300

    Create link entity

---
+++ scrapper/src/main/java/edu/java/scrapper/domain/entity/LinkEntity.java
@@ -0,0 +1,33 @@
+package edu.java.scrapper.domain.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import java.time.OffsetDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LinkEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "link_id")
+    private Long linkId;
+    private String url;
+    @Column(name = "updated_at")
+    private OffsetDateTime updatedAt;
+    @ManyToMany(mappedBy = "links", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private Set<ChatEntity> chats = new HashSet<>();
+}
