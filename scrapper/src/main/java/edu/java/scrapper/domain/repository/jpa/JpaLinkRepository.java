c196e872e5b31d7ff3aad27db3c4f6a987ef5f21
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-24 23:21:04 +0300

    Add findOlderThan method

--- scrapper/src/main/java/edu/java/scrapper/domain/repository/jpa/JpaLinkRepository.java
+++ scrapper/src/main/java/edu/java/scrapper/domain/repository/jpa/JpaLinkRepository.java
@@ -1,12 +1,18 @@
 package edu.java.scrapper.domain.repository.jpa;
 
 import edu.java.scrapper.domain.entity.LinkEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.time.OffsetDateTime;
+import java.util.Collection;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JpaLinkRepository extends JpaRepository<LinkEntity, Long> {
     Optional<LinkEntity> findByUrl(String url);
 
+    @Query("SELECT le FROM LinkEntity le WHERE le.updatedAt < :cutoff")
+    Collection<LinkEntity> findOlderThan(@Param("cutoff") OffsetDateTime cutoff);
 }
