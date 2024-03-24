9cc8e4520c2dece63b55665d7427f56abedaae00
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-24 23:20:43 +0300

    Implement getOlderThan, updateLink and getChatsForLink

--- scrapper/src/main/java/edu/java/scrapper/service/jpa/JpaLinkService.java
+++ scrapper/src/main/java/edu/java/scrapper/service/jpa/JpaLinkService.java
@@ -1,27 +1,33 @@
 package edu.java.scrapper.service.jpa;
 
+import edu.java.scrapper.domain.dto.ChatDto;
 import edu.java.scrapper.domain.dto.LinkDto;
 import edu.java.scrapper.domain.entity.ChatEntity;
 import edu.java.scrapper.domain.entity.LinkEntity;
 import edu.java.scrapper.domain.repository.jpa.JpaChatRepository;
 import edu.java.scrapper.domain.repository.jpa.JpaLinkRepository;
-import edu.java.scrapper.service.LinkFactory;
 import edu.java.scrapper.service.LinkService;
+import edu.java.scrapper.service.factory.ChatFactory;
+import edu.java.scrapper.service.factory.LinkFactory;
 import java.net.URI;
+import java.time.OffsetDateTime;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class JpaLinkService implements LinkService {
     private final JpaLinkRepository jpaLinkRepository;
     private final JpaChatRepository jpaChatRepository;
+    private static final String CHAT_NOT_FOUND_TEMPLATE = "Chat with id %d not found!";
+    private static final String LINK_NOT_FOUND_TEMPLATE = "Link not found!";
 
     @Override
     public LinkDto add(long tgChatId, URI url) {
         LinkEntity link = jpaLinkRepository.findByUrl(url.toString()).orElse(LinkFactory.createLinkEntity(url));
         ChatEntity chat = jpaChatRepository.findById(tgChatId)
-            .orElseThrow(() -> new RuntimeException("Chat with id " + tgChatId + "not found!"));
+            .orElseThrow(() -> new RuntimeException(String.format(CHAT_NOT_FOUND_TEMPLATE, tgChatId)));
 
         chat.getLinks().add(link);
         link.getChats().add(chat);
@@ -35,7 +41,7 @@ public class JpaLinkService implements LinkService {
     public LinkDto remove(long tgChatId, URI url) {
         LinkEntity link = jpaLinkRepository.findByUrl(url.toString()).orElse(LinkFactory.createLinkEntity(url));
         ChatEntity chat = jpaChatRepository.findById(tgChatId)
-            .orElseThrow(() -> new RuntimeException("Chat with id " + tgChatId + "not found!"));
+            .orElseThrow(() -> new RuntimeException(String.format(CHAT_NOT_FOUND_TEMPLATE, tgChatId)));
 
         chat.getLinks().remove(link);
         link.getChats().remove(chat);
@@ -48,7 +54,7 @@ public class JpaLinkService implements LinkService {
     @Override
     public Collection<LinkDto> listAll(long tgChatId) {
         ChatEntity chat = jpaChatRepository.findById(tgChatId)
-            .orElseThrow(() -> new RuntimeException("Chat with id " + tgChatId + "not found!"));
+            .orElseThrow(() -> new RuntimeException(String.format(CHAT_NOT_FOUND_TEMPLATE, tgChatId)));
 
         Collection<LinkDto> links = new HashSet<>();
         for (LinkEntity linkEntity : chat.getLinks()) {
@@ -56,5 +62,38 @@ public class JpaLinkService implements LinkService {
         }
 
         return links;
+    }
+
+    @Override
+    public Collection<LinkDto> getOlderThan(int minutes) {
+        Collection<LinkEntity> linkEntities =
+            jpaLinkRepository.findOlderThan(OffsetDateTime.now().minusMinutes(minutes));
+        return linkEntities.stream()
+            .map(LinkFactory::entityToDto)
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public void updateLink(LinkDto linkDto) {
+        LinkEntity linkEntity = jpaLinkRepository.findById(linkDto.getLinkId())
+            .orElseThrow(() -> new RuntimeException(LINK_NOT_FOUND_TEMPLATE));
+
+        linkEntity.setUrl(linkDto.getUrl());
+        linkEntity.setUpdatedAt(linkDto.getUpdatedAt());
+
+        jpaLinkRepository.save(linkEntity);
+    }
+
+    @Override
+    public Collection<ChatDto> getChatsForLink(LinkDto link) {
+        LinkEntity linkEntity =
+            jpaLinkRepository.findById(link.getLinkId())
+                .orElseThrow(() -> new RuntimeException(LINK_NOT_FOUND_TEMPLATE));
+
+        Collection<ChatDto> chats = new HashSet<>();
+        for (ChatEntity chatEntity : linkEntity.getChats()) {
+            chats.add(ChatFactory.entityToDto(chatEntity));
+        }
+        return chats;
     }
 }
