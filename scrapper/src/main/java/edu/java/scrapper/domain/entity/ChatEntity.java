b453e85faa69df08e1fd30c911aa612b9bd90091
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-24 23:14:53 +0300

    Change generation type and delete referencedColumnName

--- scrapper/src/main/java/edu/java/scrapper/domain/entity/ChatEntity.java
+++ scrapper/src/main/java/edu/java/scrapper/domain/entity/ChatEntity.java
@@ -11,13 +11,13 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import java.time.OffsetDateTime;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import java.time.OffsetDateTime;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Getter
@@ -28,14 +28,14 @@ import java.util.Set;
 public class ChatEntity {
     @Id
     @Column(name = "chat_id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long chatId;
     @Column(name = "registered_at")
     private OffsetDateTime registeredAt;
     @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinTable(name = "links_to_chats",
-               joinColumns = @JoinColumn(name = "chat_id", referencedColumnName = "chatId"),
-               inverseJoinColumns = @JoinColumn(name = "link_id", referencedColumnName = "linkId"))
+               joinColumns = @JoinColumn(name = "chat_id"),
+               inverseJoinColumns = @JoinColumn(name = "link_id"))
     private Set<LinkEntity> links = new HashSet<>();
 
 }
