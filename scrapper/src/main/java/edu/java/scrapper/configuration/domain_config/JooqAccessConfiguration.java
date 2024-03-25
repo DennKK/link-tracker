f688cfeb4cba9e7a7c28c068267548dce9550328
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-25 19:33:07 +0300

    Add JOOQ service bean configurations

--- scrapper/src/main/java/edu/java/scrapper/configuration/domain_config/JooqAccessConfiguration.java
+++ scrapper/src/main/java/edu/java/scrapper/configuration/domain_config/JooqAccessConfiguration.java
@@ -1,4 +1,25 @@
 package edu.java.scrapper.configuration.domain_config;
 
+import edu.java.scrapper.domain.repository.jooq.JooqChatRepository;
+import edu.java.scrapper.domain.repository.jooq.JooqLinkRepository;
+import edu.java.scrapper.service.ChatService;
+import edu.java.scrapper.service.LinkService;
+import edu.java.scrapper.service.jooq.JooqChatService;
+import edu.java.scrapper.service.jooq.JooqLinkService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(prefix = "app", name = "database-access-type", havingValue = "jooq")
 public class JooqAccessConfiguration {
+    @Bean
+    public LinkService linkService(JooqLinkRepository linkRepository) {
+        return new JooqLinkService(linkRepository);
+    }
+
+    @Bean
+    public ChatService chatService(JooqChatRepository chatRepository) {
+        return new JooqChatService(chatRepository);
+    }
 }
