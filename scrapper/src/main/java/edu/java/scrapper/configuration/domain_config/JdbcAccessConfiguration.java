a559a0662a1d7100fedf1cd48efdc59d18994821
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-25 19:33:18 +0300

    Add JDBC service bean configurations

--- scrapper/src/main/java/edu/java/scrapper/configuration/domain_config/JdbcAccessConfiguration.java
+++ scrapper/src/main/java/edu/java/scrapper/configuration/domain_config/JdbcAccessConfiguration.java
@@ -1,4 +1,25 @@
 package edu.java.scrapper.configuration.domain_config;
 
+import edu.java.scrapper.domain.repository.jdbc.JdbcChatRepository;
+import edu.java.scrapper.domain.repository.jdbc.JdbcLinkRepository;
+import edu.java.scrapper.service.ChatService;
+import edu.java.scrapper.service.LinkService;
+import edu.java.scrapper.service.jdbc.JdbcChatService;
+import edu.java.scrapper.service.jdbc.JdbcLinkService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(prefix = "app", name = "database-access-type", havingValue = "jdbc")
 public class JdbcAccessConfiguration {
+    @Bean
+    public LinkService linkService(JdbcLinkRepository linkRepository) {
+        return new JdbcLinkService(linkRepository);
+    }
+
+    @Bean
+    public ChatService chatService(JdbcChatRepository chatRepository) {
+        return new JdbcChatService(chatRepository);
+    }
 }
