18fd957c2e433a5d043b9058dfa4fce8821f9c3b
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-26 19:00:54 +0300

    Add find and add test

---
+++ scrapper/src/test/java/edu/java/scrapper/domain/repository/jooq/JooqChatRepositoryTest.java
@@ -0,0 +1,41 @@
+package edu.java.scrapper.domain.repository.jooq;
+
+import edu.java.scrapper.IntegrationEnvironment;
+import edu.java.scrapper.domain.dto.ChatDto;
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+public class JooqChatRepositoryTest extends IntegrationEnvironment {
+    @Autowired
+    private JooqChatRepository chatRepository;
+
+    @Test
+    @Transactional
+    @Rollback
+    public void findAndAddTest() {
+        List<ChatDto> chats = new ArrayList<>();
+        chats.add(new ChatDto(null, OffsetDateTime.now()));
+        chats.add(new ChatDto(null, OffsetDateTime.now()));
+
+        for (ChatDto chat : chats) {
+            chatRepository.add(chat);
+        }
+
+        List<ChatDto> chatsFromDb = (List<ChatDto>) chatRepository.findAll();
+        Assertions.assertEquals(chats.size(), chatsFromDb.size());
+        for (int i = 0; i < chats.size(); i++) {
+            long difference =
+                ChronoUnit.SECONDS.between(chats.get(i).getRegisteredAt(), chatsFromDb.get(i).getRegisteredAt());
+            Assertions.assertTrue(Math.abs(difference) < 5); // Acceptable difference of less than 5 seconds
+        }
+    }
+}
