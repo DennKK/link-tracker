096b438764c7d30445780eaf34628332804adb1f
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-28 17:50:42 +0300

    Refactor listALl, remove, and add to use existing chat in db

--- scrapper/src/main/java/edu/java/scrapper/service/jooq/JooqLinkService.java
+++ scrapper/src/main/java/edu/java/scrapper/service/jooq/JooqLinkService.java
@@ -2,6 +2,7 @@ package edu.java.scrapper.service.jooq;
 
 import edu.java.scrapper.domain.dto.ChatDto;
 import edu.java.scrapper.domain.dto.LinkDto;
+import edu.java.scrapper.domain.repository.jooq.JooqChatRepository;
 import edu.java.scrapper.domain.repository.jooq.JooqLinkRepository;
 import edu.java.scrapper.service.LinkService;
 import java.net.URI;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class JooqLinkService implements LinkService {
     private final JooqLinkRepository jooqLinkRepository;
+    private final JooqChatRepository jooqChatRepository;
 
     @Override
     public LinkDto add(long tgChatId, URI url) {
@@ -19,20 +21,24 @@ public class JooqLinkService implements LinkService {
         link.setUrl(String.valueOf(url));
         link.setUpdatedAt(OffsetDateTime.now());
         jooqLinkRepository.add(link);
-        jooqLinkRepository.map(link, new ChatDto(tgChatId, null));
+        LinkDto linkFromDb = jooqLinkRepository.getByUrl(link.getUrl());
+        ChatDto chatFromDb = jooqChatRepository.findByTgChatId(tgChatId);
+        jooqLinkRepository.map(linkFromDb.getLinkId(), chatFromDb.getChatId());
         return link;
     }
 
     @Override
     public LinkDto remove(long tgChatId, URI url) {
         LinkDto link = jooqLinkRepository.getByUrl(url.toString());
-        jooqLinkRepository.unmap(link.getLinkId(), tgChatId);
+        ChatDto chat = jooqChatRepository.findByTgChatId(tgChatId);
+        jooqLinkRepository.unmap(link.getLinkId(), chat.getChatId());
         return link;
     }
 
     @Override
     public Collection<LinkDto> listAll(long tgChatId) {
-        return jooqLinkRepository.findAllByChat(new ChatDto(tgChatId, null));
+        ChatDto chat = jooqChatRepository.findByTgChatId(tgChatId);
+        return jooqLinkRepository.findAllByChat(chat);
     }
 
     @Override
