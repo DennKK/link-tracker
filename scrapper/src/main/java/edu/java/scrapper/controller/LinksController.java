1aafde979b19ad7b43bc973183dd865ddd74cb51
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-28 18:16:35 +0300

    Reorder annotations

--- scrapper/src/main/java/edu/java/scrapper/controller/LinksController.java
+++ scrapper/src/main/java/edu/java/scrapper/controller/LinksController.java
@@ -14,9 +14,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-@Slf4j
 public class LinksController implements LinkApi {
     private final LinkService service;
 
