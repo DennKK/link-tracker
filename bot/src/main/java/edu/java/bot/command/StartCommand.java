fa4154960c26e3df199a3834e652a42530a27dee
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-28 18:28:54 +0300

    Add scrapper client interaction

--- bot/src/main/java/edu/java/bot/command/StartCommand.java
+++ bot/src/main/java/edu/java/bot/command/StartCommand.java
@@ -2,10 +2,16 @@ package edu.java.bot.command;
 
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
+import edu.java.bot.client.ScrapperClient;
+import edu.java.bot.client.exception.ScrapperClientException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class StartCommand implements Command {
+    private final ScrapperClient scrapperClient;
+
     @Override
     public String command() {
         return "/start";
@@ -19,9 +25,20 @@ public class StartCommand implements Command {
     @Override
     public SendMessage handle(Update update) {
         long chatId = update.message().chat().id();
+
+        String replyMessage =
+            "Hi! \nThis bot allows you to track changes on Github and Stackoverflow! \nUse /help for more information";
+
+        try {
+            scrapperClient.addChat(chatId);
+        } catch (ScrapperClientException exception) {
+            replyMessage = exception.getMessage();
+        } catch (RuntimeException exception) {
+            replyMessage = "Ooops! Something went wrong!";
+        }
+
         return new SendMessage(
-            chatId,
-            "Hi! \nThis bot allows you to track changes on Github and Stackoverflow! \nUse /help for more information"
+            chatId, replyMessage
         );
     }
 }
