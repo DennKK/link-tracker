bc20249924b526a6320c99a49d119536082d3494
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-29 19:03:58 +0300

    Refactor to use link processors

--- scrapper/src/main/java/edu/java/scheduler/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/scheduler/LinkUpdaterScheduler.java
@@ -1,27 +1,24 @@
 package edu.java.scheduler;
 
-import edu.java.client.github.GitHubClient;
 import edu.java.client.tgbot.BotClient;
-import edu.java.scrapper.domain.dto.ChatDto;
+import edu.java.processor.LinkUpdateProcessor;
 import edu.java.scrapper.domain.dto.LinkDto;
 import edu.java.scrapper.service.LinkService;
-import java.time.OffsetDateTime;
 import java.util.Collection;
 import java.util.List;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@EnableScheduling
+@Slf4j
 @Component
+@EnableScheduling
 @RequiredArgsConstructor
 public class LinkUpdaterScheduler implements LinkUpdater {
-    private static final Logger LOGGER = Logger.getLogger(LinkUpdaterScheduler.class.getName());
-    private GitHubClient gitHubClient;
+    private final List<LinkUpdateProcessor> processors;
     @Value("${app.update-frequency}")
     protected int updateFrequency;
     private LinkService linkService;
@@ -30,28 +27,21 @@ public class LinkUpdaterScheduler implements LinkUpdater {
     @Override
     @Scheduled(fixedDelayString = "${app.scheduler.interval}")
     public void update() {
-        LOGGER.info("Running link update task...");
+        log.info("Running link update task...");
 
         Collection<LinkDto> oldLinks = linkService.getOlderThan(updateFrequency);
-        for (LinkDto link : oldLinks) {
-            OffsetDateTime lastTimeUpdated = getLastUpdateTime(link);
-            if (lastTimeUpdated.isAfter(link.getUpdatedAt())) {
-                Collection<ChatDto> chats = linkService.getChatsForLink(link);
-                List<Long> chatIds = chats.stream().map(ChatDto::getChatId).collect(Collectors.toList());
-                botClient.sendUpdateToBot(link, chatIds);
-                link.setCheckedAt(OffsetDateTime.now());
-                link.setUpdatedAt(lastTimeUpdated);
-                linkService.updateLastCheckTime(link);
-                linkService.refreshLinkActivity(link);
-            }
-        }
-    }
 
-    // TODO: Create url parser
-    // В предыдущих заданиях нигде не было необходимости парсить ссылки
-    // Хотелось бы отличать ссылку на stackoverflow и github друга от друга и от других ссылок
-    // Распарсиватель ссылок пока не готов, поэтому оставляю заглушку
-    private OffsetDateTime getLastUpdateTime(LinkDto link) {
-        return OffsetDateTime.parse(gitHubClient.getGitHubResponse("DennKK", "link-tracker").updatedAt());
+        oldLinks.forEach(link -> {
+            try {
+                LinkUpdateProcessor processor = processors.stream()
+                    .filter(p -> p.supports(link.getUrl()))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalStateException("No processor found for link: " + link.getUrl()));
+
+                processor.processLinkUpdate(link);
+            } catch (Exception e) {
+                log.error("Error processing link {}: {}", link.getUrl(), e.getMessage());
+            }
+        });
     }
 }
