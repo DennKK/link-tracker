e5e648a9e303c40fed5957dc0b90b6c15e670dce
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-29 18:16:32 +0300

    Add new field checked at

--- migrations/create_links.sql
+++ migrations/create_links.sql
@@ -2,5 +2,6 @@ CREATE TABLE links
 (
     link_id    bigserial PRIMARY KEY,
     url        varchar(255)             not null,
+    checked_at timestamp with time zone not null,
     updated_at timestamp with time zone not null
 );
