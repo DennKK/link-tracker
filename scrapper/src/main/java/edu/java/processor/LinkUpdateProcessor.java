930dcb077c1bf345936f27806ff453f754ce5f15
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-29 19:04:26 +0300

    Add getLastActivityTime and processLinkUpdate methods

--- scrapper/src/main/java/edu/java/processor/LinkUpdateProcessor.java
+++ scrapper/src/main/java/edu/java/processor/LinkUpdateProcessor.java
@@ -1,5 +1,12 @@
 package edu.java.processor;
 
+import edu.java.scrapper.domain.dto.LinkDto;
+import java.time.OffsetDateTime;
+
 public interface LinkUpdateProcessor {
     boolean supports(String url);
+
+    OffsetDateTime getLastActivityTime(String url);
+
+    void processLinkUpdate(LinkDto linkDto);
 }
