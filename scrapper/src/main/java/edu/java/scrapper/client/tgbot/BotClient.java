61021394d9393707553955e6fb8a7560234941d3
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-31 16:09:17 +0300

    Add more logging

--- scrapper/src/main/java/edu/java/scrapper/client/tgbot/BotClient.java
+++ scrapper/src/main/java/edu/java/scrapper/client/tgbot/BotClient.java
@@ -1,37 +1,43 @@
-package edu.java.client.tgbot;
+package edu.java.scrapper.client.tgbot;
 
-import edu.java.client.exception.BotClientException;
 import edu.java.payload.dto.request.LinkUpdateRequest;
 import edu.java.payload.dto.response.ApiErrorResponse;
+import edu.java.scrapper.client.exception.BotClientException;
 import edu.java.scrapper.domain.dto.LinkDto;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-@Service
+@Slf4j
 public class BotClient {
     private final WebClient webClient;
 
-    public BotClient(@Qualifier("telegramBotClient") WebClient webClient) {
-        this.webClient = webClient;
+    public BotClient(@NotNull String baseUrl) {
+        webClient = WebClient.builder().baseUrl(baseUrl).build();
     }
 
     public void sendUpdateToBot(LinkDto link, List<Long> chatIds) {
+        log.info("Sending update to bot for Link ID: {}, URL: {}", link.getLinkId(), link.getUrl());
         LinkUpdateRequest request = new LinkUpdateRequest(link.getLinkId(), link.getUrl(), "New update", chatIds);
         update(request);
     }
 
     public void update(LinkUpdateRequest linkUpdateRequest) {
+        log.info("Preparing to post update: {}", linkUpdateRequest);
         webClient.post().uri("/updates")
             .body(BodyInserters.fromValue(linkUpdateRequest))
             .retrieve()
             .onStatus(HttpStatusCode::isError, response -> response.bodyToMono(ApiErrorResponse.class)
-                .flatMap(error -> Mono.error(new BotClientException(error.exceptionMessage()))))
+                .flatMap(error -> {
+                    log.error("Error updating link: {}", error.exceptionMessage());
+                    return Mono.error(new BotClientException(error.exceptionMessage()));
+                }))
             .bodyToMono(Void.class)
+            .doOnError(e -> log.error("Error sending update to bot", e))
             .block();
     }
 }
