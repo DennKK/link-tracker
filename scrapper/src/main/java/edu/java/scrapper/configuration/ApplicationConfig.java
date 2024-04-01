e827fbeeb5bdb6aac4f61663628690c562360649
Denis Karpov <karpovdenis.al@gmail.com>
2024-04-01 20:26:35 +0300

    Add retry config, linear, constant, exponential records

--- scrapper/src/main/java/edu/java/scrapper/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/scrapper/configuration/ApplicationConfig.java
@@ -3,6 +3,7 @@ package edu.java.scrapper.configuration;
 import edu.java.scrapper.configuration.domain.AccessType;
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
+import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
@@ -13,8 +14,31 @@ public record ApplicationConfig(
     @NotNull
     AccessType accessType,
     @NotNull
-    Scheduler scheduler
+    Scheduler scheduler,
+    @NotNull
+    RetryConfig retry
 ) {
     public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
+    }
+
+    public record RetryConfig(
+        boolean enable,
+        @NotNull String policy,
+        @NotNull
+        List<Integer> statusCodes,
+        Linear linear,
+        Constant constant,
+        Exponential exponential
+    ) {
+    }
+
+    public record Linear(@NotNull int attempts, @NotNull Duration initialBackoff, @NotNull Duration maxBackoff) {
+    }
+
+    public record Constant(@NotNull int attempts, @NotNull Duration backoff) {
+    }
+
+    public record Exponential(@NotNull int attempts, @NotNull Duration initialBackoff, @NotNull Duration maxBackoff,
+                              @NotNull double jitter) {
     }
 }
