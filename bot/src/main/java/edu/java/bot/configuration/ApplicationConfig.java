14eab0a2203d1550d192cda0f2bbe1df8c07bf30
Denis Karpov <karpovdenis.al@gmail.com>
2024-04-02 11:09:28 +0300

    Add retry field and retry config record to bot application config

--- bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
+++ bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
@@ -1,6 +1,9 @@
 package edu.java.bot.configuration;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import java.time.Duration;
+import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
@@ -8,6 +11,29 @@ import org.springframework.validation.annotation.Validated;
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(
     @NotEmpty
-    String telegramToken
+    String telegramToken,
+    @NotNull
+    RetryConfig retry
+
 ) {
+    public record RetryConfig(
+        boolean enable,
+        @NotNull String policy,
+        @NotNull
+        List<Integer> statusCodes,
+        Linear linear,
+        Constant constant,
+        Exponential exponential
+    ) {
+    }
+
+    public record Linear(@NotNull int attempts, @NotNull Duration initialBackoff, @NotNull Duration maxBackoff) {
+    }
+
+    public record Constant(@NotNull int attempts, @NotNull Duration backoff) {
+    }
+
+    public record Exponential(@NotNull int attempts, @NotNull Duration initialBackoff, @NotNull Duration maxBackoff,
+                              @NotNull double jitter) {
+    }
 }
