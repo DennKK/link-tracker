8e88a44897cb395e19dbe222faad072ef3320e7f
Denis Karpov <karpovdenis.al@gmail.com>
2024-04-07 15:03:48 +0300

    Separate records

--- bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
+++ bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
@@ -1,9 +1,8 @@
 package edu.java.bot.configuration;
 
+import edu.java.bot.configuration.retry.RetryConfig;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import java.time.Duration;
-import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
@@ -14,26 +13,5 @@ public record ApplicationConfig(
     String telegramToken,
     @NotNull
     RetryConfig retry
-
 ) {
-    public record RetryConfig(
-        boolean enable,
-        @NotNull String policy,
-        @NotNull
-        List<Integer> statusCodes,
-        Linear linear,
-        Constant constant,
-        Exponential exponential
-    ) {
-    }
-
-    public record Linear(@NotNull int attempts, @NotNull Duration initialBackoff, @NotNull Duration maxBackoff) {
-    }
-
-    public record Constant(@NotNull int attempts, @NotNull Duration backoff) {
-    }
-
-    public record Exponential(@NotNull int attempts, @NotNull Duration initialBackoff, @NotNull Duration maxBackoff,
-                              @NotNull double jitter) {
-    }
 }
