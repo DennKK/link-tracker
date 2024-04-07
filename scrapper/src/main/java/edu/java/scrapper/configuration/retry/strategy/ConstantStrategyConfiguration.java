c6e5adce812c768bce8503866b762ca85728fc0a
Denis Karpov <karpovdenis.al@gmail.com>
2024-04-07 14:30:53 +0300

    Move to strategy

--- scrapper/src/main/java/edu/java/scrapper/configuration/retry/strategy/ConstantStrategyConfiguration.java
+++ scrapper/src/main/java/edu/java/scrapper/configuration/retry/strategy/ConstantStrategyConfiguration.java
@@ -1,4 +1,4 @@
-package edu.java.scrapper.configuration.retry;
+package edu.java.scrapper.configuration.retry.strategy;
 
 import edu.java.retry.strategy.ConstantRetryStrategy;
 import edu.java.retry.strategy.RetryStrategy;
--- scrapper/src/main/java/edu/java/scrapper/configuration/retry/strategy/StrategyType.java
+++ scrapper/src/main/java/edu/java/scrapper/configuration/retry/strategy/StrategyType.java
@@ -1,4 +1,4 @@
-package edu.java.scrapper.configuration.retry;
+package edu.java.scrapper.configuration.retry.strategy;
 
 public enum StrategyType {
     CONSTANT, LINEAR, EXPONENTIAL
