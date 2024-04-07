00bd0db4c7611dcdb93c7f9d29b811f66b5376b1
Denis Karpov <karpovdenis.al@gmail.com>
2024-04-07 14:33:00 +0300

    Separate records into separate files

--- scrapper/src/main/java/edu/java/scrapper/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/scrapper/configuration/ApplicationConfig.java
@@ -1,9 +1,9 @@
 package edu.java.scrapper.configuration;
 
 import edu.java.scrapper.configuration.domain.AccessType;
+import edu.java.scrapper.configuration.retry.RetryConfig;
+import edu.java.scrapper.configuration.scheduler.Scheduler;
 import jakarta.validation.constraints.NotNull;
-import java.time.Duration;
-import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
@@ -24,27 +24,4 @@ public record ApplicationConfig(
     @NotNull
     long nanoInSeconds
 ) {
-    public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
-    }
-
-    public record RetryConfig(
-        boolean enable,
-        @NotNull String policy,
-        @NotNull
-        List<Integer> statusCodes,
-        Linear linear,
-        Constant constant,
-        Exponential exponential
-    ) {
-    }
-
-    public record Linear(@NotNull int attempts, @NotNull Duration initialBackoff, @NotNull Duration maxBackoff) {
-    }
-
-    public record Constant(@NotNull int attempts, @NotNull Duration backoff) {
-    }
-
-    public record Exponential(@NotNull int attempts, @NotNull Duration initialBackoff, @NotNull Duration maxBackoff,
-                              @NotNull double jitter) {
-    }
 }
