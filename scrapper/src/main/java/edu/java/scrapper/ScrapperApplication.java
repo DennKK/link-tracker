78f0fec5a4975055fc726a216fbb81e852594012
Denis Karpov <karpovdenis.al@gmail.com>
2024-04-07 14:23:31 +0300

    Add kafka property config class to EnableConfigurationProperties annotation

--- scrapper/src/main/java/edu/java/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/scrapper/ScrapperApplication.java
@@ -1,6 +1,7 @@
 package edu.java.scrapper;
 
 import edu.java.scrapper.configuration.ApplicationConfig;
+import edu.java.scrapper.configuration.kafka.KafkaPropertyConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @Slf4j
 @SpringBootApplication
-@EnableConfigurationProperties(ApplicationConfig.class)
+@EnableConfigurationProperties({ApplicationConfig.class, KafkaPropertyConfig.class})
 public class ScrapperApplication {
     public static void main(String[] args) {
         var context = SpringApplication.run(ScrapperApplication.class, args);
