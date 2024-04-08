7b38a017469937a6d610d570541b84a5a0c400a8
Denis Karpov <karpovdenis.al@gmail.com>
2024-04-08 16:17:19 +0300

    Add logging

--- scrapper/src/main/java/edu/java/scrapper/service/notification/QueueNotificationSenderService.java
+++ scrapper/src/main/java/edu/java/scrapper/service/notification/QueueNotificationSenderService.java
@@ -3,15 +3,18 @@ package edu.java.scrapper.service.notification;
 import edu.java.payload.dto.request.LinkUpdateRequest;
 import edu.java.scrapper.service.queue.ScrapperQueueProducer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class QueueNotificationService implements NotificationService {
+public class QueueNotificationSenderService implements NotificationSenderService {
     private final ScrapperQueueProducer queueProducer;
 
     @Override
     public void sendNotification(LinkUpdateRequest updateRequest) {
+        log.info("Sending message to queue");
         queueProducer.send(updateRequest);
     }
 }
