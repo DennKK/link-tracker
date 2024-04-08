5a5ce18a5426440af474a0de35e850c881dcdc67
Denis Karpov <karpovdenis.al@gmail.com>
2024-04-08 16:12:09 +0300

    Add logging

--- scrapper/src/main/java/edu/java/scrapper/service/notification/HttpNotificationSenderService.java
+++ scrapper/src/main/java/edu/java/scrapper/service/notification/HttpNotificationSenderService.java
@@ -3,15 +3,18 @@ package edu.java.scrapper.service.notification;
 import edu.java.payload.dto.request.LinkUpdateRequest;
 import edu.java.scrapper.client.tgbot.BotClient;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class HttpNotificationService implements NotificationService {
+public class HttpNotificationSenderService implements NotificationSenderService {
     private final BotClient botClient;
 
     @Override
     public void sendNotification(LinkUpdateRequest update) {
+        log.info("Sending notification to the bot");
         botClient.sendUpdateToBot(update);
     }
 }
