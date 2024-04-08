263174b312d821b919a007d4ad353114ba33276a
Denis Karpov <karpovdenis.al@gmail.com>
2024-04-08 16:14:03 +0300

    Add valueDeserializer method

--- scrapper/src/main/java/edu/java/scrapper/configuration/kafka/KafkaConsumerConfiguration.java
+++ scrapper/src/main/java/edu/java/scrapper/configuration/kafka/KafkaConsumerConfiguration.java
@@ -4,6 +4,7 @@ import edu.java.payload.dto.request.LinkUpdateRequest;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 @EnableKafka
 @Configuration
@@ -21,33 +23,31 @@ public class KafkaConsumerConfiguration {
     private String groupId;
     @Value("${app.kafka.consumer.auto-offset-reset}")
     private String autoOffsetReset;
-    @Value("${app.kafka.consumer.key-deserializer}")
-    private String keyDeserializer;
-    @Value("${app.kafka.consumer.value-deserializer}")
-    private String valueDeserializer;
 
     @Bean
-    ConcurrentKafkaListenerContainerFactory<String, LinkUpdateRequest> kafkaListenerContainerFactory(
-        ConsumerFactory<String, LinkUpdateRequest> consumerFactory
-    ) {
+    public ConcurrentKafkaListenerContainerFactory<String, LinkUpdateRequest> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, LinkUpdateRequest> factory =
             new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory);
+        factory.setConsumerFactory(consumerFactory());
         return factory;
     }
 
     @Bean
     public ConsumerFactory<String, LinkUpdateRequest> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerProps());
+        return new DefaultKafkaConsumerFactory<>(consumerProps(), new StringDeserializer(), valueDeserializer());
     }
 
     private Map<String, Object> consumerProps() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keyDeserializer);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueDeserializer);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
         return props;
+    }
+
+    private JsonDeserializer<LinkUpdateRequest> valueDeserializer() {
+        JsonDeserializer<LinkUpdateRequest> deserializer = new JsonDeserializer<>(LinkUpdateRequest.class);
+        deserializer.addTrustedPackages("*");
+        return deserializer;
     }
 }
