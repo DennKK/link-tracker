2a8b6eac90a40b7078da16dc8e0c586f2ff4fb76
Denis Karpov <karpovdenis.al@gmail.com>
2024-04-15 17:11:56 +0300

    Update tests due message counter

--- bot/src/test/java/edu/java/bot/ListenerBotTest.java
+++ bot/src/test/java/edu/java/bot/ListenerBotTest.java
@@ -5,22 +5,23 @@ import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.utility.BotUtils;
 import edu.java.bot.command.Command;
 import edu.java.bot.command.HelpCommand;
-import edu.java.bot.command.ListCommand;
 import edu.java.bot.command.StartCommand;
 import edu.java.bot.command.UntrackCommand;
 import edu.java.bot.message.MessageProcessor;
+import edu.java.bot.metric.MessageCounter;
 import java.util.List;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 public class ListenerBotTest {
     @Test
     public void unknownCommandTest() {
         MessageProcessor messageProcessor = new MessageProcessor(List.of(new StartCommand(null)));
-        ListenerBot listenerBot = new ListenerBot(null, messageProcessor);
+        ListenerBot listenerBot = new ListenerBot(null, messageProcessor, mock(MessageCounter.class));
         String testCommand = "/someCommand";
 
         Update update = BotUtils.parseUpdate("{\"message\":{\"text\":\"" + testCommand + "\",\"chat\":{\"id\":0}}}");
@@ -29,18 +30,6 @@ public class ListenerBotTest {
 
         String text = message.getParameters().get("text").toString();
         assertEquals("There is no such command as " + testCommand, text);
-    }
-
-    @Test
-    public void listCommandTest() {
-        MessageProcessor messageProcessor = new MessageProcessor(List.of(new ListCommand(null)));
-        ListenerBot listenerBot = new ListenerBot(null, messageProcessor);
-        String testCommand = "/list";
-
-        Update update = BotUtils.parseUpdate("{\"message\":{\"text\":\"" + testCommand + "\",\"chat\":{\"id\":0}}}");
-
-        SendMessage message = listenerBot.process(update);
-        assertEquals("There is no links yet :(", message.getParameters().get("text").toString());
     }
 
     @Test
@@ -56,7 +45,7 @@ public class ListenerBotTest {
             );
 
         MessageProcessor mp = new MessageProcessor(commands);
-        ListenerBot bot = new ListenerBot(null, mp);
+        ListenerBot bot = new ListenerBot(null, mp, mock(MessageCounter.class));
 
         String testCommand = "/help";
 
